314b62d8ea1ee3ff2748aa997c0d552c5951ba59
Richard Rembert <info@rembertdesigns.co>
2022-04-17 13:21:02 -0400

    JS intervals

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// add quick timer: 1min 5min etc.
+// add status bar
+
+var time = document.getElementById("time");
+var minute = document.getElementById("min");
+var second = document.getElementById("sec");
+var startButton = document.getElementById("start");
+var resetButton = document.getElementById("reset");
+var seti = undefined;
+var mm = "25";
+var ss = "00";
