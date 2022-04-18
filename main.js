89d1057a6855797c4a117fd366655fab5cc7e672
Richard Rembert <info@rembertdesigns.co>
2022-04-18 13:23:28 -0400

    Adding more JS

--- main.js
+++ main.js
@@ -10,3 +10,45 @@ var resetButton = document.getElementById("reset");
 var seti = undefined;
 var mm = "25";
 var ss = "00";
+
+startButton.addEventListener("click", function start () {
+    if (startButton.innerHTML === "START") {
+      startButton.innerHTML = "PAUSE";
+      mm = minute.value;
+      ss = second.value;
+      if (minute.value === "") minute.value = "00";
+      if (second.value === "") second.value = "00";
+      minute.setAttribute("disabled", true);
+      second.setAttribute("disabled", true);
+      seti = setInterval(function () {
+        if (second.value > 0) {
+          second.value -= 1;
+          if (second.value < 10 && second.value >= 0) {
+            second.value = "0" + second.value;
+          }
+        }
+        else if (minute.value > 0) {
+          second.value = "59";
+          minute.value -= 1;
+          if (minute.value < 10 && minute.value >= 0) {
+            minute.value = "0" + minute.value;
+          }
+        }
+        else {
+          clearInterval(seti);
+          document.body.style.backgroundImage = "linear-gradient(to top left, #c0392b, #e74c3c , #9b59b6)";
+          setTimeout(function() {
+            alert("Time Out !");
+            res();
+          }, 100);
+        }
+      }, 1000);
+    }
+    else {
+      minute.removeAttribute("disabled");
+      second.removeAttribute("disabled");
+      startButton.innerHTML = "START";
+      clearInterval(seti);
+    }
+  });
+  
